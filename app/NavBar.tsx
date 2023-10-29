7c8e63c011a4c2282708b1f88a74fe7781d0c1c7
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-30 09:19:51 +1100

    add login,logout link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,17 +20,26 @@ const NavBar = () => {
         </Link>
         <ul className="flex space-x-6">
           {links.map((link) => (
-            <Link
-              key={link.label}
-              href={link.href}
-              className={`${
-                link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
-              } hover:text-zinc-800 transition-colors`}
-            >
-              {link.label}
-            </Link>
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={`${
+                  link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+                } hover:text-zinc-800 transition-colors`}
+              >
+                {link.label}
+              </Link>
+            </li>
           ))}
         </ul>
+        <Box>
+          {status === "authenticated" && (
+            <Link href="/api/auth/signout">Signout</Link>
+          )}
+          {status === "unauthenticated" && (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
+        </Box>
       </nav>
     </header>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Container, Theme } from "@radix-ui/themes";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,13 +26,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body>
-        <Theme>
-          <NavBar />
+        <AuthProvider>
+          <Theme>
+            <NavBar />
 
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
