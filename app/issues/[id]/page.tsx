df0a285229a96bc4a81f4ae3bf05b49823af98b2
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-29 16:21:33 +1100

    style issue detail component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
+import { Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 
 const IssueDetailPage = async ({
@@ -14,8 +16,11 @@ const IssueDetailPage = async ({
   if (!issue) return notFound();
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
     </div>
   );
 };
