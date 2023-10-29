e9e40f749530ff485df4e171ab326e80edb53aff
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-29 16:45:03 +1100

    add additional loading skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,22 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import "react-loading-skeleton/dist/skeleton.css";
+import { Heading, Flex, Card, Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import Markdown from "react-markdown";
+
 const LoadingIssueDetail = () => {
-  return <div>dd</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex className="space-x-3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose mt-4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetail;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import delay from "delay";
 import { notFound } from "next/navigation";
 import Markdown from "react-markdown";
 
@@ -15,6 +16,7 @@ const IssueDetailPage = async ({
     },
   });
   if (!issue) return notFound();
+  await delay(2000);
   return (
     <div>
       <Heading>{issue.title}</Heading>
