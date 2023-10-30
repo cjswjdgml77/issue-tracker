7e9856611177d2335e4ff6231e6a2a4d8b46c64c
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-30 14:42:49 +1100

    sort issue

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -20,8 +20,14 @@ const IssuesPage = async ({
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+  const orderBy = columes
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: { status: status },
+    orderBy: orderBy,
   });
 
   return (
