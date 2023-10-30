f4a7b7cc0a03a3ef654ac4b83f714b63a4ce2f39
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-30 22:00:53 +1100

    Build the issue chart

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import LatestIssues from "./LatestIssues";
 import Pagenation from "./components/Pagenation";
 import IssueSummary from "./IssueSummery";
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -12,7 +13,7 @@ export default async function Home() {
   return (
     <>
       <LatestIssues />
-      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <IssueChart open={open} inProgress={inProgress} closed={closed} />
     </>
   );
 }
