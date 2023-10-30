d917a1b629c453558f8017e20a98defc35854115
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-30 16:22:56 +1100

    build pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagenation from "./components/Pagenation";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return <Pagenation itemCount={100} pageSize={10} currentPage={10} />;
 }
