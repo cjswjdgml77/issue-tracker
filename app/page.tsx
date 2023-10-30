0fa0a77673619da18c872b6ed821ca07a4b0d886
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-30 21:51:11 +1100

    build issue summary;

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import prisma from "@/prisma/client";
 import LatestIssues from "./LatestIssues";
 import Pagenation from "./components/Pagenation";
+import IssueSummary from "./IssueSummery";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return (
+    <>
+      <LatestIssues />
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+    </>
+  );
 }
