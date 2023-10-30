84db2168380fc850dd522ef1ca8eed90587d6998
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-30 14:00:26 +1100

    organize import assigneeselect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,22 +1,13 @@
 "use client";
+import { Skeleton } from "@/app/components/";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Skeleton } from "@/app/components/";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ["users"],
-    queryFn: () => axios.get<User[]>("/api/user").then((res) => res.data),
-    staleTime: 60 * 1000, // 60s
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
   if (isLoading) return <Skeleton />;
   if (error) return null;
   return (
@@ -50,5 +41,11 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
-
+const useUsers = () =>
+  useQuery({
+    queryKey: ["users"],
+    queryFn: () => axios.get<User[]>("/api/user").then((res) => res.data),
+    staleTime: 60 * 60 * 1000, // 60mins
+    retry: 3,
+  });
 export default AssigneeSelect;
