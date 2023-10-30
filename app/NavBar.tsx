0b1f824ddb460f910e6bb54a1c04915b94233932
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-10-30 14:09:40 +1100

    build filter component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -36,7 +36,7 @@ const NavLink = () => {
   const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
+    { label: "Issues", href: "/issues/list" },
   ];
   return (
     <ul className="flex space-x-6">
